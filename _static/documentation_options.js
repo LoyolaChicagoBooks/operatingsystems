b2bb6ed56691f5f1718571e12bbb9d25b033ce14
gkthiruvathukal <gkthiruvathukal@users.noreply.github.com>
2021-11-27 15:52:11 +0000

    deploy: 3fa05b8bad75227cf0b4191aefa42d7391e2219a

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Updated 2021-11-27',
+    VERSION: '2021-11-27',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
